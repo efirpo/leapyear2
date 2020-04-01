9d31fe267e77ec3168433299d180a263075d499f
Mark Gridley <mark_gridley@yahoo.com>
2020-04-01 09:53:03 -0700

    add functionality in scripts.js and polish style of page

---
+++ js/scripts.js
@@ -0,0 +1,34 @@
+
+// back-end logic
+
+var leapYear = function(year) {
+  if ((year % 4 === 0) && (year % 100 !== 0) 
+  || (year % 400 === 0)) {
+    return true;
+  }
+  else { 
+    return false;
+  }
+};
+
+
+// front-end logic
+
+$(document).ready(function() {
+  $("form#leap-year").submit(function(event) {
+    event.preventDefault();
+    var year = parseInt($("input#year").val());
+    var result = leapYear(year);
+    
+    $(".year").text(year);
+
+    if(!result) {
+      $(".not").html("<strong>not</strong>"); 
+    } else {
+      $(".not").text("");
+    }
+
+    $("#result").show();
+
+  });
+});
